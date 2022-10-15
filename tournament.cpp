50a585f3047e06b390230f3158ce04557b2aa013
Jag114 <49473034+Jag114@users.noreply.github.com>
2022-10-15 19:51:01 +0200

    using references now, i think

--- tournament.cpp
+++ tournament.cpp
@@ -4,50 +4,83 @@
 class Warrior {
 public:
     std::string Name;
-    unsigned int Hp;
-    unsigned int Atk;
-    unsigned int availableStatPoints;
+    unsigned int Hp = 1;
+    unsigned int Atk = 1;
+    unsigned int availableStatPoints = 10;
+    unsigned int hpToPointsRatio = 2;
+    unsigned int atkToPointsRatio = 1;
 };
 
-void getStats(Warrior object) {
+int getStats(Warrior &object) {
     std::cout << "Name: " << object.Name << "\n" << "Health points: " << object.Hp << "\n" << "Atk points: " << object.Atk <<"\n";
     std::cout << "Stat points left: " << object.availableStatPoints << std::endl;
+    return 0;
 }
 
-unsigned int changeStat(int statPoint, int ratio) {
+int changeStat(int type, Warrior &object) {
     int addedStats;
-    if (statPoint > 0) {
-        std::cout << "You have " << statPoint << " points to redistribute for your gladiator's statistics" << "\n";
+    if (object.availableStatPoints > 0) {
+        std::cout << "You have " << object.availableStatPoints << " points to redistribute for your gladiator's statistics" << "\n";
         std::cout << "How many of them do you want to use to enhance Hp?" << std::endl;
         std::cin >> addedStats;
-        statPoint--;
+        object.availableStatPoints = object.availableStatPoints - addedStats;
+        switch (type)
+        {
+        case 1:
+            object.Atk = addedStats * object.atkToPointsRatio;
+            break;
+        case 2:
+            object.Hp = addedStats * object.hpToPointsRatio;
+            break;
+        default:
+            break;
+        }
     }
     else {
-        std::cout << "Too few stat points" << std::endl;
+         std::cout << "Too few stat points" << std::endl;
+        return 0;
     }
-    return addedStats * ratio;
+
+    return 0;
 }
 
-std::string changeName() {
+int changeName(Warrior &object) {
     std::string name;
     std::cout << "Set your gladiator's name: ";
     std::cin >> name;
-    return name;
+    object.Name = name;
+    std::cout << "Your gladiator's name is: " << object.Name << "\n";
+    return 0;
 }
 
 
 int main()
 {   
-    unsigned int hpToPointsRatio = 2;
-    unsigned int atkToPointsRatio = 1;
-
     Warrior test;
-    test.availableStatPoints = 10;
-    test.Name = changeName(); 
-    std::cout << "Your gladiator's name is: " << test.Name << "\n";
-    test.Hp = changeStat(test.availableStatPoints, hpToPointsRatio);
-    test.Atk = changeStat(test.availableStatPoints, atkToPointsRatio);
+    changeName(test);
+    for (;;) {
+        int option;
+        printf("Main Menu\n");
+        printf("1. Character stats\n2. Enchance stats\n3. Quit\n");
+        std::cin >> option;
+        switch (option)
+        {
+        case 1:
+            getStats(test);
+            break;
+        case 2:
+            changeStat(1, test);
+            changeStat(2, test);
+            break;
+        case 3:
+            return 0;
+            break;
+        default:
+            break;
+        }
+    }
+    
    
-    getStats(test);
+    return 0;
 }
 
